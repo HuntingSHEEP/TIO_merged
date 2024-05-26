567747aeec0113b957fa0ed9ed10e0bf9b31fcf5
zephyrus <a.glowacki98@gmail.com>
2024-05-26 22:43:09 +0200

    Rodzielenie renderowania tylko dla funkcji 2 wymiarowowych

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 int main(){
     try{
         //Wybór funkcji testowej
-        FunctionInfo funkcjaTestowaInfo = boothInfo;
+        FunctionInfo funkcjaTestowaInfo = schwefelInfo;
 
         //Liczba wymiarów
         const size_t dims = 2;
@@ -20,45 +20,69 @@ int main(){
         //Liczba mrówek
         const int antsCount = 5;
 
-        //Algorytm mrówkowy
-        APIAntAlgorithm<dims>* antAlgorithm = new APIAntAlgorithm<dims>(
-            antsCount
-            , std::function<double(double*, size_t)>(funkcjaTestowaInfo.functionPointer)
-            , FunDomain<dims>(funkcjaTestowaInfo.xMin, funkcjaTestowaInfo.xMax)
-        );
+        if(dims == 2){
+            //Algorytm mrówkowy
+            APIAntAlgorithm<2>* antAlgorithm2 = new APIAntAlgorithm<2>(
+                antsCount
+                , std::function<double(double*, size_t)>(funkcjaTestowaInfo.functionPointer)
+                , FunDomain<2>(funkcjaTestowaInfo.xMin, funkcjaTestowaInfo.xMax)
+            );
+            
+            AnthillRenderer* anthillRenderer = new AnthillRenderer(funkcjaTestowaInfo, antsCount, antAlgorithm2);
+            double timeToNextUpdate = 0.f;
 
-        AnthillRenderer* anthillRenderer = new AnthillRenderer(funkcjaTestowaInfo, antsCount, antAlgorithm);
-        double timeToNextUpdate = 0.f;
+            while(anthillRenderer->windowStillOpened()){
+                anthillRenderer->enginePoll();
 
-        while(anthillRenderer->windowStillOpened()){
-            anthillRenderer->enginePoll();
+                if(timeToNextUpdate < 0.25f){
+                    timeToNextUpdate += anthillRenderer->getDeltaTime();
+                }
+                else if(!antAlgorithm2->finished()){
+                    antAlgorithm2->update();
 
-            if(timeToNextUpdate < 0.25f){
-                timeToNextUpdate += anthillRenderer->getDeltaTime();
-            }
-            else if(!antAlgorithm->finished()){
-                antAlgorithm->update();
-
-                // After the algorithm finished its work, display the best place found and it's coordinates
-                if(antAlgorithm->finished())
-                {
-                    auto [point, value] = antAlgorithm->getBest();
-                    std::cout << "Best value found in point (";
-                    for(int i = 0; i < dims; i++)
+                    // After the algorithm finished its work, display the best place found and it's coordinates
+                    if(antAlgorithm2->finished())
                     {
-                        std::cout << point.pos[i] << ( i != dims - 1 ? ", " : "");
+                        auto [point, value] = antAlgorithm2->getBest();
+                        std::cout << "Best value found in point (";
+                        for(int i = 0; i < dims; i++)
+                        {
+                            std::cout << point.pos[i] << ( i != dims - 1 ? ", " : "");
+                        }
+                        std::cout << ") with value: " << value << ".\n";
                     }
-                    std::cout << ") with value: " << value << ".\n";
+
+                    anthillRenderer->update(antAlgorithm2->getAntsPositions(), antAlgorithm2->getNest());
+                    timeToNextUpdate = 0.f;
                 }
 
-                anthillRenderer->update(antAlgorithm->getAntsPositions(), antAlgorithm->getNest());
-                timeToNextUpdate = 0.f;
+                anthillRenderer->draw();
             }
+            anthillRenderer->finish();
+        }else{
+            //Algorytm mrówkowy
+            APIAntAlgorithm<dims>* antAlgorithm = new APIAntAlgorithm<dims>(
+                antsCount
+                , std::function<double(double*, size_t)>(funkcjaTestowaInfo.functionPointer)
+                , FunDomain<dims>(funkcjaTestowaInfo.xMin, funkcjaTestowaInfo.xMax)
+            );
 
-            anthillRenderer->draw();
+            while(!antAlgorithm->finished())
+                antAlgorithm->update();
+            
+            // After the algorithm finished its work, display the best place found and it's coordinates
+            if(antAlgorithm->finished())
+            {
+                auto [point, value] = antAlgorithm->getBest();
+                std::cout << "Best value found in point (";
+                for(int i = 0; i < dims; i++)
+                {
+                    std::cout << point.pos[i] << ( i != dims - 1 ? ", " : "");
+                }
+                std::cout << ") with value: " << value << ".\n";
+                getchar();
+            }
         }
-
-        anthillRenderer->finish();
     }
     catch(const std::exception& e){std::cout<< "ERROR: "<<e.what()<<std::endl;}
     catch(...){std::cout<< "Unknown error"<<std::endl;}
